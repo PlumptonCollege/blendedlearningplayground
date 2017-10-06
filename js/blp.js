aa7c584f0876fe882ae9a560fdbfd2898ebf31ca
James Michael Maltby <jamesmaltby@me.com>
2017-10-06 14:36:04 +0100

    Add files via upload

--- js/blp.js
+++ js/blp.js
@@ -1,4 +1,18 @@
 (function() {
   
+$(document).ready(function(){
+    $('.filters__filter').click(function() {
+      filter = $(this);
+      if( $(this).hasClass('is-active') ) return;
+      parent = $(this).parent();
+      previous = $(parent).find('.is-active');
+      previous.removeClass('is-active');
+      $(this).addClass('is-active');
+      console.log(previous.data('filter'));
+      $('#cards').removeClass( previous.data('filter') );
+      $('#cards').addClass( $(this).data('filter') );
+  });
+});
+  
   
 })();
